9611e81195e9a1836fd4164a85212d388462982e
Kriwn <nonisgod@gmail.com>
2026-04-16 20:56:18 +0700

    fix(dashboaed): route page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 import { RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { HeroSearchComponent } from '../hero-search/hero-search.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   templateUrl: './dashboard.component.html',
   styleUrls: [ './dashboard.component.css' ],
-  imports: [RouterLink],
+  imports: [RouterLink, NgFor, HeroSearchComponent],
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
